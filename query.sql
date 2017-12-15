d8302709646e367a7b2b1caecadb0737115b4f24
Meghanakanakavidu <meghanakanakavidu@gmail.com>
2017-12-15 08:43:56 +0000

    first commit

---
+++ query.sql
@@ -0,0 +1,3 @@
+Drop table pet
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20))
+SELECT * from sys.objects
---
+++ t1.bat
@@ -0,0 +1,2 @@
+Echo off
+sqlcmd -s EC2AMAZ-R1KQBRD\SQLEXPRESS -d MEGHANA -i C:\Users\Administrator\Desktop\query.sql
