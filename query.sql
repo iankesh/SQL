bcb9cc92d3cc0e68eef4dc8b4611408d045c9f78
Meghanakanakavidu <meghana.kanakavidu@gmail.com>
2017-12-19 00:08:35 +0530

    changes

--- query.sql
+++ query.sql
@@ -1,3 +1,6 @@
-Drop table pet
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20))
-SELECT * from sys.objects
+drop table if exists test
+create table test(
+name varchar(20),
+age numeric(10)
+)
+select * from sys.objects 
